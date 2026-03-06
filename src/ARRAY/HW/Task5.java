7a7db619dfc6d5292f5ad9601c864fa89905fa24
Mansur <mansurvampir@gmail.com>
2026-03-06 23:50:45 +0300

    5 задание дз(массиввы)

--- src/ARRAY/HW/Task5.java
+++ src/ARRAY/HW/Task5.java
@@ -1,36 +1,75 @@
-package ARRAY.HW;
+import STRING.classWork.Task2;
 
 public class Task5 {
     static void main() {
-        task5();
-    }
+        int[][] board ={
+                {2, 0, 2},
+                {2, 2, 0},
+                {2, 1, 2}
+        };
+        checkWinner(board);}
 
-        public static void task5() {
-            int[][] board = {
-                    {2, 0, 2},
-                    {2, 2, 0},
-                    {2, 1, 2}
-            };
 
-            int winner = 0;
-            for (int i = 0; i < 3; i++) {
-                if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
-                    winner = board[i][0];
+public static void checkWinner(int[][] board) {
+    int n = board.length;
+    int winner = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (board[i][0] != 0) {
+            boolean rowWin = true;
+            for (int j = 1; j < n; j++) {
+                if (board[i][j] != board[i][0]) {
+                    rowWin = false;
+                    break;
                 }
-                if (board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
-                    winner = board[0][i];
-                }
             }
-            if (winner == 0 && board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-                winner = board[0][0];
-            }
-            if (winner == 0 && board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-                winner = board[0][2];
-            }
-            if (winner != 0) {
-                System.out.println("Победил игрок №" + winner);
-            } else {
-                System.out.println("Победителя пока нет или ничья.");
+            if (rowWin){
+                winner= board[i][0];
             }
         }
     }
+
+    if (winner == 0) {
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] != 0) {
+                boolean colWin = true;
+                for (int i = 1; i < n; i++) {
+                    if (board[i][j] != board[0][j]) {
+                        colWin = false;
+                        break;
+                    }
+                }
+                if (colWin) winner = board[0][j];
+            }
+        }
+    }
+
+    if (winner == 0 && board[0][0] != 0) {
+        boolean diagWin = true;
+        for (int i = 1; i < n; i++) {
+            if (board[i][i] != board[0][0]) {
+                diagWin = false;
+                break;
+            }
+        }
+        if (diagWin) winner = board[0][0];
+    }
+
+    if (winner == 0 && board[0][n - 1] != 0) {
+        boolean sideDiagWin = true;
+        for (int i = 1; i < n; i++) {
+            if (board[i][n - 1 - i] != board[0][n - 1]) {
+                sideDiagWin = false;
+                break;
+            }
+        }
+        if (sideDiagWin) winner = board[0][n - 1];
+    }
+
+    if (winner != 0) {
+        System.out.println("Победил игрок №" + winner);
+    } else {
+        System.out.println("Победителя нет.");
+    }
+}}
+
