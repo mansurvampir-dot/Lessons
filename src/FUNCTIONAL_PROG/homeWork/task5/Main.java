c397a959416abbec04e51d6f814a7814149d4cc7
Mansur <mansurvampir@gmail.com>
2026-05-12 21:16:41 +0300

    домашняя работа(Generics )

--- src/FUNCTIONAL_PROG/homeWork/task5/Main.java
+++ src/FUNCTIONAL_PROG/homeWork/task5/Main.java
@@ -4,11 +4,14 @@ import java.util.stream.Collector;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args){
+    static void main(String[] args) {
         Collector<Double, Stats[], Stats> statsCollector = Collector.of(
                 () -> new Stats[]{Stats.empty()},
                 (a, c) -> a[0] = a[0].merge(c),
-                (a, b) -> { a[0] = a[0].combine(b[0]); return a; },
+                (a, b) -> {
+                    a[0] = a[0].combine(b[0]);
+                    return a;
+                },
                 a -> a[0]
         );
         Stats stats = Stream.of(3.0, 1.0, 4.0, 1.0, 5.0, 9.0)
--- src/FUNCTIONAL_PROG/homeWork/task5/Stats.java
+++ src/FUNCTIONAL_PROG/homeWork/task5/Stats.java
@@ -1,14 +1,16 @@
 package FUNCTIONAL_PROG.homeWork.task5;
 
 record Stats(long count, double sum, double min, double max) {
-    double avg() {
-        if (count==0){
-    return 0;}else{
-        return sum/count;}
-    }
-
     static Stats empty() {
         return new Stats(0, 0, Double.MAX_VALUE, Double.MIN_VALUE);
+    }
+
+    double avg() {
+        if (count == 0) {
+            return 0;
+        } else {
+            return sum / count;
+        }
     }
 
     Stats merge(double v) {
