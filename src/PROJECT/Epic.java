cceeded8f618e71f0da50f7f1ab3e96979eaa11b
Mansur <mansurvampir@gmail.com>
2026-05-12 21:42:06 +0300

    проект

---
+++ src/PROJECT/Epic.java
@@ -0,0 +1,24 @@
+package PROJECT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Epic extends Task{
+    private final List<Integer> subtaskIds = new ArrayList<>();
+
+    public Epic(String title, String description) {
+        super(title, description);
+    }
+
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    public void addSubtaskId(int id) {
+        subtaskIds.add(id);
+    }
+
+    public void removeSubtaskId(int id) {
+        subtaskIds.remove(Integer.valueOf(id));
+    }
+}
---
+++ src/PROJECT/Task.java
@@ -0,0 +1,58 @@
+package PROJECT;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.status = TaskStatus.NEW;
+    }
+
+    public enum TaskStatus {NEW, IN_PROGRESS, DONE}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
+
